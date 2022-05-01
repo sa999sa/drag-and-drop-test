c6bf6cc9957af4ac2be51dd6a1887587d3035d98
sa999na <lumimosita999@icloud.com>
2022-05-01 17:37:48 +0900

    first commit

--- src/components/piece.tsx
+++ src/components/piece.tsx
@@ -1,1 +1,16 @@
 import React from "react";
+import { useState } from "react";
+
+type Position={
+    xRate:number;
+    yRate:number;
+};
+
+export const Piece=()=>{
+    const [currentPosition,setCurrentPosition]=useState<Position>({
+        xRate:150,
+        yRate:150
+    });
+
+    const onDrag=(e:DraggableEvent)
+}
